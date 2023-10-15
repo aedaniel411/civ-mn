f723b1659015329e74b4c2075355b1db28fcb2be
Daniel Acosta <aedaniel@gmail.com>
2023-10-15 14:17:01 -0600

    231015

--- 23b/tema2/f.m
+++ 23b/tema2/f.m
@@ -1,3 +1,4 @@
 function xr = f(x)
-  xr = x^3 - x - 1;
+%  xr = x^3 - x - 1;
+  xr = exp(x-1) - 1.5 * x;
 end
--- 23b/tema2/fp.m
+++ 23b/tema2/fp.m
@@ -1,3 +1,4 @@
 function xr = fp(x)
-  xr = 3*x^2 - 1;
+  %xr = 3*x^2 - 1;
+  xr = exp(x-1) - 1.5
 end
